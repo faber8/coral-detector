456d7d34d9044823068efa02dcaefdee74568bc4
Faber8 <faber8@hotmail.com>
2026-06-27 16:43:14 +0200

    Switch to standard TFLite model

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ CONFIDENCE = float(os.getenv('CONFIDENCE', '0.65'))
 PERSON_CLASS = int(os.getenv('PERSON_CLASS', '0'))
 MAX_RESULTS = int(os.getenv('MAX_RESULTS', '10'))
 
-MODEL_PATH = MODELS_DIR / 'mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite'
+MODEL_PATH = MODELS_DIR / 'mobilenet_ssd_v2_coco_quant_postprocess.tflite'
 LABELS_PATH = MODELS_DIR / 'coco_labels.txt'
 
 
@@ -18,7 +18,7 @@ def ensure_model_files() -> None:
     if not MODEL_PATH.exists():
         try:
             urllib.request.urlretrieve(
-                'https://github.com/google-coral/test_data/raw/master/ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite',
+                'https://github.com/google-coral/test_data/raw/master/ssd_mobilenet_v2_coco_quant_postprocess.tflite',
                 MODEL_PATH,
             )
         except Exception:
